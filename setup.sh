fbd04998cc8515cb5f3d9845bc5a3d90a286b455
Pavan Sai Sheshetti <70468631+PavanSaiSheshetti@users.noreply.github.com>
2021-05-09 16:43:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email=\"your-email@domain.com\"\n\
+email=\"sheshettipavansai969@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo"\
 [server]\n\
